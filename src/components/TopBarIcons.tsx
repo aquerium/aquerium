9a4665a49b9d6a01adba5fb5239e432b38a19df6
David <t-daestr@microsoft.com>
2019-07-31 12:26:56 -0700

    Center icons in top bar and add menu.

--- src/components/TopBarIcons.tsx
+++ src/components/TopBarIcons.tsx
@@ -1,50 +1,96 @@
-import * as React from 'react';
-import { Image, IImageProps, ImageFit, Stack, Link, CommandBarButton } from 'office-ui-fabric-react';
+import * as React from "react";
+import {
+  Image,
+  IImageProps,
+  ImageFit,
+  Stack,
+  Link,
+  CommandBarButton
+} from "office-ui-fabric-react";
+import { HomeUIContext } from "./HomeUIContext";
 
-class TopBarIcons extends React.Component<{}> {
-  render() {
-    const imageProps: IImageProps = {
-      src: "GlitterboxLogo2.png",
-      imageFit: ImageFit.centerContain,
-      maximizeFrame: true,
-      width: 90,
-      height: 90
-    };
+const TopBarIcons = () => {
+  const imageProps: IImageProps = {
+    src: "GlitterboxLogo2.png",
+    imageFit: ImageFit.centerContain,
+    maximizeFrame: true,
+    width: 80,
+    height: 80
+  };
 
-    return (
-      <Stack horizontal horizontalAlign="space-evenly" verticalAlign="center" styles={{root:{background: '#faf9f8', width: 430}}}>
-        <div></div>
-        <Link href="https://github.com" target='_blank' >
-          <Image {...imageProps as any} title="My GitHub Home"/>
-        </Link>
-        <CommandBarButton menuIconProps={{iconName: "More" }} title="Options" 
-          styles={{root:{width:48, height: 48, background: '#faf9f8'}, menuIcon:{fontSize:48, color: '#1B3E74'}}} 
-          persistMenu={false}
-          menuProps={{ items: this.getMenuItems(), shouldFocusOnMount: true,
-                      shouldFocusOnContainer: true}}
-        />
-      </Stack>
-    );
-  }
+  const { isEditing, setEditing } = React.useContext(HomeUIContext);
 
-  private getMenuItems = () => {
+  const getMenuItems = () => {
     return [
       {
-        key: 'sign out',
-        name: 'Sign Out',
+        key: "sign out",
+        name: "Sign Out",
         iconProps: {
-          iconName: 'SignOut'
+          iconName: "SignOut"
         }
       },
       {
-        key: 'edit queries',
-        name: 'Edit Queries',
+        key: "edit queries",
+        name: "Edit Queries",
         iconProps: {
-          iconName: 'Edit'
+          iconName: "Edit"
+        },
+        onclick: () => {
+          setEditing(!isEditing);
         }
       }
-    ]
-  }
-}
+    ];
+  };
+
+  return (
+    <Stack
+      horizontal
+      verticalAlign="center"
+      styles={{
+        root: {
+          background: "#faf9f8",
+          width: "290px",
+          height: "80",
+          position: "relative"
+        }
+      }}
+    >
+      <Link
+        href="https://github.com"
+        target="_blank"
+        styles={{
+          root: {
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-61%)"
+          }
+        }}
+      >
+        <Image {...imageProps as any} title="My GitHub Home" />
+      </Link>
+      <CommandBarButton
+        menuIconProps={{ iconName: "More" }}
+        title="Options"
+        styles={{
+          root: {
+            width: 24,
+            height: 24,
+            background: "#faf9f8",
+            margin: "auto",
+            position: "absolute",
+            right: "20px"
+          },
+          menuIcon: { fontSize: 24, color: "#1b374" }
+        }}
+        persistMenu={false}
+        menuProps={{
+          items: getMenuItems(),
+          shouldFocusOnMount: true,
+          shouldFocusOnContainer: true
+        }}
+      />
+    </Stack>
+  );
+};
 
 export default TopBarIcons;
