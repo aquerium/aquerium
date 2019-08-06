0b2204bec97e03bec94e30d61abbe49993d0ea6e
Trip Master <t-damast@microsoft.com>
2019-08-06 11:23:47 -0700

    clean up app.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,9 @@
 import React from "react";
-import { connect } from "react-redux";
 import { HomeUI } from "./HomeUI";
 import { initializeIcons } from "@uifabric/icons";
-import { formProperties } from "@uifabric/utilities";
+
 initializeIcons();
 
-// export class App extends React.Component<{}> {
-//   public render(): JSX.Element {
-//     return (
-//       <HomeUI />
-//       //<LoginUI />
-//     );
-//   }
-// }
-
-function AppView() {
+export function App() {
   return <HomeUI />;
 }
-
-export const App = connect()(AppView);
