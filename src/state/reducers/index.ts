4229e85de7e0f0a848c20ea1998b95d01f1625ab
Trip Master <t-damast@microsoft.com>
2019-08-08 13:05:35 -0700

    fix comment

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,4 +1,4 @@
 import { combineReducers } from "redux";
-import { queryList } from "./queryList"; //hehe can't write this yet! We need import what we're writing in the reducer
+import { queryList } from "./queryList";
 
 export const rootReducer = combineReducers({ queryList });
