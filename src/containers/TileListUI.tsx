bdc7c02c44d796924a9c00200d6dae762e9b9548
Trip Master <t-damast@microsoft.com>
2019-08-08 10:51:53 -0700

    Move to a containers folder, added jsdoc

---
+++ src/containers/TileListUI.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { TaskTile } from "../components/TaskTile";
+import { TaskTileClassNames } from "../components/TaskTile.ClassNames";
+import { connect } from "react-redux";
+import { IState, queryListType } from "../state/state.types";
+
+/**
+ * This interface defines the props object passed into TileListComponent
+ *
+ * @interface
+ */
+interface ITileListUIProps {
+  /**
+   * @property { queryListType } queryList
+   * The queryList map that is passed in, element at a time, to be rendered by TaskTile
+   */
+  queryList: queryListType;
+}
+
+function TileListComponent(props: ITileListUIProps) {
+  return (
+    <div className={TaskTileClassNames.root}>
+      <div className={TaskTileClassNames.listContainer}>
+        {Object.keys(props.queryList).map(key => (
+          <TaskTile item={props.queryList[key]} key={key} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state: IState) => ({
+  queryList: state.queryList
+});
+
+export const TileListUI = connect(mapStateToProps)(TileListComponent);
