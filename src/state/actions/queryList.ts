5f195dee494a583eca03b4c510d4b1f9f72cb050
Trip Master <t-damast@microsoft.com>
2019-08-08 10:53:30 -0700

    fix documentation

--- src/state/actions/queryList.ts
+++ src/state/actions/queryList.ts
@@ -1,14 +1,14 @@
 import { IQuery } from "../state.types";
 
-/**This file is responsible for actions that modify the state's queryList, like
+/*This file is responsible for actions that modify the state's queryList, like
  * addQuery
  * removeQuery
- * editQuery
  */
 
 /**
- * Action to add a query to the queryList
- * @param query
+ * Action creator to add a query to the queryList
+ * @param { IQuery } query
+ * The particular query to be added
  */
 export const addQuery = (query: IQuery) => ({
   type: "ADD_QUERY",
@@ -16,8 +16,9 @@ export const addQuery = (query: IQuery) => ({
 });
 
 /**
- * Action to specified query from queryList
- * @param query
+ * Action creator to specified query from queryList
+ * @param {string} queryName
+ * The name of the query to be removed
  */
 export const removeQuery = (queryName: string) => ({
   type: "REMOVE_QUERY",
