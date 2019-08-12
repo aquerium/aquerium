77d3ef0ea7f8420a45272a404bddc7cf78e09fdb
Trip Master <t-damast@microsoft.com>
2019-08-12 09:26:32 -0700

    update changeUI with immutability helpers

--- src/state/actions/changeUI.ts
+++ src/state/actions/changeUI.ts
@@ -1,26 +1,32 @@
 /*This file is responsible for actions that are realated the state's current UI
+ * login
+ * logout
  */
 
 /**
- * @type { type: string} the action type for a login action
+ * @type { type: string } the action type for a login action
  */
-export type changeUILoginAction = { type: string };
+export type changeUILoginAction = { type: string; change: string };
 
 /**
  * @type { type: string} the action type for a logout action
  */
-export type changeUILogoutAction = { type: string };
+export type changeUILogoutAction = { type: string; change: string };
 
-/*
- * Upon correct validation, creates an action to send the user from login UI to home UI
+/**
+ * Action creator to send the user from login UI to home UI
+ * @property { string } change the UI screen that will be changed to
  */
 export const login = () => ({
-  type: "LOGIN"
+  type: "LOGIN",
+  change: "Home"
 });
 
-/*
- * Upon correct validation, creates an action to send the user from home UI to login UI
+/**
+ * Action creator to send the user from home UI to login UI
+ * @property { string } change the UI screen that will be changed to
  */
 export const logout = () => ({
-  type: "LOGOUT"
+  type: "LOGOUT",
+  change: "Login"
 });
--- src/state/reducers/changeUI.ts
+++ src/state/reducers/changeUI.ts
@@ -1,3 +1,5 @@
+import update from "immutability-helper";
+import { changeUILoginAction, changeUILogoutAction } from "../actions/changeUI";
 /*
  * This file is responsible for the reducers that deal with the UI
  */
@@ -5,20 +7,31 @@
 const DEFAULT_STATE: { currUI: string } = { currUI: "Login" };
 
 /**
- * This reducer deals with UI and storing the Token
- * @param cur_UI
+ * This reducer deals with changing the UI
+ * @param currUI the current UI being shown. This needs to be updated to change the UI
  */
-export const changeUI = (state: { currUI: string } = DEFAULT_STATE, action: any) => {
+export const changeUI = (
+  state: { currUI: string } = DEFAULT_STATE,
+  action: changeUILoginAction | changeUILogoutAction
+) => {
   switch (action.type) {
     case "LOGIN": {
-      const updatedState = { ...state };
-      updatedState.currUI = "Home";
-      return updatedState;
+      const nextUI: string = (action as changeUILoginAction).change;
+      return update(state, {
+        $set: {
+          ...state,
+          currUI: nextUI
+        }
+      });
     }
     case "LOGOUT": {
-      const updatedState = { ...state };
-      updatedState.currUI = "Login";
-      return updatedState;
+      const nextUI: string = (action as changeUILogoutAction).change;
+      return update(state, {
+        $set: {
+          ...state,
+          currUI: nextUI
+        }
+      });
     }
     default:
       return state;
