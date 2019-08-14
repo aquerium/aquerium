3f794ebd8d5ecb6ecc479ec85ea96d95f41aae04
Trip Master <t-damast@microsoft.com>
2019-08-13 17:34:03 -0700

    Add UI changes

--- src/state/actions/changeUI.ts
+++ src/state/actions/changeUI.ts
@@ -9,35 +9,35 @@ export type changeUILoginAction = { type: string };
 export type changeUILogoutAction = { type: string };
 
 /**
- * Action creator to send the user from login UI to home UI.
+ * Action creator to send the user from login UI to Home UI.
  */
 export const login = () => ({
   type: "LOGIN"
 });
 
 /**
- * Action creator to send the user from home UI to login UI.
+ * Action creator to send the user from home UI to Login UI.
  */
 export const logout = () => ({
   type: "LOGOUT"
 });
 
 /**
- * Action creator to send the user to the Edit Query UI.
+ * Action creator to send the user to Edit Query UI.
  */
 export const toEditQuery = () => ({
   type: "EDIT"
 });
 
 /**
- * Action creator to send the user to the QueryList UI.
+ * Action creator to send the user to QueryList UI.
  */
 export const toQueryList = () => ({
   type: "QUERY"
 });
 
 /**
- * Action creator to send the user to the home UI
+ * Action creator to send the user to Home UI
  */
 export const toHome = () => ({
   type: "HOME"
