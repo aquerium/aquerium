a2fac4b2c7d62f7d2db85f50a2646de165db29db
Cathy Zhang <cathystars11@gmail.com>
2019-08-15 10:05:54 -0700

    Reorder imports

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,5 +1,5 @@
-import fetch from "isomorphic-fetch";
 import { IQuery, IUserInfo } from "../state";
+import fetch from "isomorphic-fetch";
 
 const GIST_NAME = "aquerium_helper.json";
 const GIST_DESCRIP = "helper gist for Aquerium";
