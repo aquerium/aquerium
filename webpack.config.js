b162f0c7777ebda3681540cc3d19e5cb030e6428
Ken <kchau@microsoft.com>
2019-08-19 15:40:09 -0700

    adding a webpack config for building background.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,29 @@
-const { webpackConfig } = require('just-scripts');
-module.exports = webpackConfig;
+const path = require("path");
+module.exports = {
+  entry: "./src/background/index.ts",
+  mode: "production",
+  devtool: "source-maps",
+  optimization: {
+    minimize: false
+  },
+  output: {
+    path: path.join(__dirname, "public/dist"),
+    filename: "background.js"
+  },
+  resolve: {
+    extensions: [".mjs", ".js", ".ts"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        loader: require.resolve("babel-loader"),
+        options: {
+          babelrc: false,
+          configFile: false,
+          presets: [require.resolve("@babel/preset-env")]
+        }
+      }
+    ]
+  }
+};
