1f109eb777f58b2febca5bd48c91695fa85d5335
David <t-daestr@microsoft.com>
2019-08-23 14:04:24 -0700

    Address comments.

--- src/components/EditQuery.tsx
+++ src/components/EditQuery.tsx
@@ -23,11 +23,11 @@ import {
 } from "./EditQueryUI.styles";
 
 enum InputStatuses {
-  /* Value indicating that the input has been validated and successfully updated to the new (or existing) query. */
+  /** Value indicating that the input has been validated and successfully updated to the new (or existing) query. */
   successfulEdit = 0,
-  /* Value indicating that the current user input is not valid. */
+  /** Value indicating that the current user input is not valid. */
   invalidEdit,
-  /* Value indicating that that the user input has been saved to the current query edits. */
+  /** Value indicating that that the user input has been saved to the current query edits. */
   saved
 }
 
@@ -41,11 +41,11 @@ interface IEditQueryUIState {
   messageType: MessageBarType;
   /** Tracks the message that should be rendered by the message bar, if needed. */
   message: string;
-  /* MessageBarButton items, if any, that are the options a user can take given a message. */
+  /** MessageBarButton items, if any, that are the options a user can take given a message. */
   actions?: JSX.Element;
-  /* Whether or not a message bar should be rendered, given the action the user wants to take. */
+  /** Whether or not a message bar should be rendered, given the action the user wants to take. */
   renderMessageBar: boolean;
-  /* Whether the review status field is enabled or not, depending on if PR's are in the query. */
+  /** Whether the review status field is enabled or not, depending on if PR's are in the query. */
   enableReviewStatusField: boolean;
   /**
    * The current selections the user is making to a query, which will be used to either construct
