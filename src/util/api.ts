f8536b11ff3027d5c313d558ab7f56f08662e462
Cathy Zhang <cathystars11@gmail.com>
2019-08-27 12:11:17 -0700

    Fix jsdoc for api.ts

--- src/util/api.ts
+++ src/util/api.ts
@@ -5,26 +5,26 @@ const GIST_NAME = "aquerium_helper.json";
 const GIST_DESCRIP = "helper gist for Aquerium";
 
 /**
- * Represents the object structure for using GitHub Gists API
+ * Represents the object structure for using GitHub Gists API.
  */
 interface IGist {
-  /** Description of the gist */
+  /** Description of the gist. */
   description: string;
-  /** Whether the gist is public or private */
+  /** Whether the gist is public or private. */
   public: boolean;
-  /** Files contained in the gist */
+  /** Files contained in the gist. */
   files: {
-    /** Name of the file */
+    /** Name of the file. */
     [key: string]: {
-      /** File contents */
+      /** File contents. */
       content: string;
     };
   };
 }
 
 /**
- * Creates a GitHub gist upon initial submission of the user's personal access token and returns the user's relevant information
- * @param token User's GitHub personal access token
+ * Creates a GitHub gist upon initial submission of the user's personal access token and returns the user's relevant information.
+ * @param token User's GitHub personal access token.
  */
 export async function createGist(token: string): Promise<{ user?: IUserInfo; errorCode?: number }> {
   try {
@@ -60,8 +60,8 @@ export async function createGist(token: string): Promise<{ user?: IUserInfo; err
 }
 
 /**
- * Returns the queryMap object in the user's gist file
- * @param user IUserInfo object with the user's relevant information
+ * Returns the queryMap object in the user's gist file.
+ * @param user Contains the user's relevant information.
  */
 export async function getQueryMapObj(
   user: IUserInfo
@@ -78,9 +78,9 @@ export async function getQueryMapObj(
 }
 
 /**
- * Updates the user's gist contents with an updated queryMap object
- * @param user IUserInfo object with the user's relevant information
- * @param queryMap Contains the user's queries in a dictionary
+ * Updates the user's gist contents with an updated queryMap object.
+ * @param user Contains the user's relevant information.
+ * @param queryMap Contains the user's queries in a dictionary.
  */
 export async function updateGist(
   user: IUserInfo,
@@ -114,8 +114,8 @@ export async function updateGist(
 }
 
 /**
- * Reads from the user's gist and returns the gist contents in IGist format
- * @param user IUserInfo object with the user's relevant information
+ * Reads from the user's gist and returns the gist contents in IGist format.
+ * @param user Contains the user's relevant information.
  */
 async function loadFromGist(user: IUserInfo): Promise<{ gist?: IGist; errorCode?: number }> {
   try {
