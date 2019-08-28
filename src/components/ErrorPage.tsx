a044ea7ee0172e278cc907508ecdee2808b86640
David <t-daestr@microsoft.com>
2019-08-28 09:41:49 -0700

    Fix changed.

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -3,30 +3,28 @@ import { Stack, Text, Icon, ActionButton } from "office-ui-fabric-react";
 
 interface IErrorPageProps {
   /** The error message to be rendered. */
-  errorString: string;
+  errorMessage: string;
 }
 
-const iconStyles = { styles: { root: { fontSize: 50, color: "#8f191b" } } };
+const errorIconStyles = { styles: { root: { fontSize: 50, color: "#8f191b" } } };
 const oopsStyles = { root: { color: "#00395c", fontSize: 48 } };
 const errorStyles = { root: { color: "#00395c", fontSize: 30 } };
-const stackProps = { root: { height: "100%" } };
+const stackStyles = { root: { height: "100%" } };
+const iconProps = { iconName: "Home" };
+const homeIconStyles = {
+  root: { fontSize: 30, transform: "translateY(200%)" },
+  icon: { fontSize: 80 }
+};
 
 export const ErrorPage = (props: IErrorPageProps): JSX.Element => {
-  const { errorString } = props;
+  const { errorMessage } = props;
 
   return (
-    <Stack horizontalAlign="center" verticalAlign="center" styles={stackProps}>
-      <Icon iconName="Error" styles={iconStyles.styles} />
+    <Stack horizontalAlign="center" verticalAlign="center" styles={stackStyles}>
+      <Icon iconName="Error" styles={errorIconStyles.styles} />
       <Text styles={oopsStyles}>Oops!</Text>
-      <Text styles={errorStyles}>{errorString}</Text>
-      <ActionButton
-        iconProps={{ iconName: "Home" }}
-        text="Return to Home"
-        styles={{
-          root: { fontSize: 30, transform: "translateY(200%)" },
-          icon: { fontSize: 80 }
-        }}
-      />
+      <Text styles={errorStyles}>{errorMessage}</Text>
+      <ActionButton iconProps={iconProps} text="Return to Home" styles={homeIconStyles} />
     </Stack>
   );
 };
