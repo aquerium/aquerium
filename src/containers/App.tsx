efcf1ab25497425de968b32554edcc2958d04f00
David <t-daestr@microsoft.com>
2019-08-30 11:02:29 -0700

    Make LoginUI default login.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -108,7 +108,7 @@ class AppView extends React.Component<IAppViewProps> {
   private _renderUI = () => {
     switch (this.props.UI) {
       case "Login": {
-        return <EditQuery />;
+        return <LoginUI />;
       }
       case "Home": {
         return <HomeUI />;
