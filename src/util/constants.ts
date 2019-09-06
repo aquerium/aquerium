03bb2437b29885b6682d20461058eae7dad0c322
Trip Master <t-damast@microsoft.com>
2019-09-06 10:58:09 -0700

    add constants

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -2,3 +2,5 @@ export const GIST_NAME = "aquerium_helper.json";
 export const GIST_DESCRIP = "helper gist for Aquerium";
 export const STATUS_CREATED = 201;
 export const STATUS_OK = 200;
+export const LOADING_PHRASE = "Just keep swimming...";
+export const QUERY_LIMIT = 100;
